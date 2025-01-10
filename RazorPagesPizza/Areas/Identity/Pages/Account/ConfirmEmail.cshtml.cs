f94bd0047ebd68910504acb98f6fe92c3cdf1c1a
thomas.wang <thomas.wang@prospa.com>
2025-01-10 12:26:58 +1100

    unit-9/add-user-claim

--- RazorPagesPizza/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ RazorPagesPizza/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using RazorPagesPizza.Areas.Identity.Data;
+using System.Security.Claims;
 
 namespace RazorPagesPizza.Areas.Identity.Pages.Account
 {
@@ -19,9 +20,13 @@ namespace RazorPagesPizza.Areas.Identity.Pages.Account
     {
         private readonly UserManager<RazorPagesPizzaUser> _userManager;
 
-        public ConfirmEmailModel(UserManager<RazorPagesPizzaUser> userManager)
+        private readonly IConfiguration _configuration;
+
+        public ConfirmEmailModel(UserManager<RazorPagesPizzaUser> userManager,
+                                    IConfiguration configuration)
         {
             _userManager = userManager;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -46,6 +51,15 @@ namespace RazorPagesPizza.Areas.Identity.Pages.Account
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
             StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+
+            var adminEmail = _configuration["AdminEmail"] ?? string.Empty;
+            if(result.Succeeded)
+            {
+                var isAdmin = string.Compare(user.Email, adminEmail, true) == 0 ? true : false;
+                await _userManager.AddClaimAsync(user, 
+                    new Claim("IsAdmin", isAdmin.ToString()));
+            }
+
             return Page();
         }
     }
